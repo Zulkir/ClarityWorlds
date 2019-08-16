6fb3ae31c56a636db0c66c485339bbf96752ba3c
Daniil Rodin <furidan@gmail.com>
2019-08-16 13:47:51 +0300

    Enabled basic numeric types for FluentGui TextBoxes and NumericUpDowns

--- Source/Clarity.Ext.Gui.EtoForms/FluentGui/FluentTextBox.cs
+++ Source/Clarity.Ext.Gui.EtoForms/FluentGui/FluentTextBox.cs
@@ -1,25 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using Eto.Forms;
 
 namespace Clarity.Ext.Gui.EtoForms.FluentGui
 {
-    public class FluentTextBox<T> : IFluentControl<T>
+    public class FluentTextBox<TObj, TVal> : IFluentControl<TObj>
     {
-        private readonly Func<T> getObject;
-        private readonly Func<T, string> getValue;
-        private readonly Action<T, string> setValue;
+        private readonly Func<TObj> getObject;
+        private readonly Func<TObj, TVal> getValue;
+        private readonly Action<TObj, TVal> setValue;
+        private readonly Func<TVal, string> valToStr;
+        private readonly Func<string, TVal> strToVal;
         private readonly TextBox etoControl;
         private bool suppressEvents;
 
         public Control EtoControl => etoControl;
         public bool IsVisible => true;
-        public T GetObject() => getObject();
+        public TObj GetObject() => getObject();
 
-        public FluentTextBox(Func<T> getObject, Func<T, string> getValue, Action<T, string> setValue)
+        public FluentTextBox(Func<TObj> getObject, 
+            Func<TObj, TVal> getValue, Action<TObj, TVal> setValue, 
+            Func<TVal, string> valToStr, Func<string, TVal> strToVal)
         {
             this.getObject = getObject;
             this.getValue = getValue;
             this.setValue = setValue;
+            this.valToStr = valToStr;
+            this.strToVal = strToVal;
             etoControl = new TextBox();
             etoControl.TextChanged += OnTextChanged;
         }
@@ -27,7 +34,7 @@ namespace Clarity.Ext.Gui.EtoForms.FluentGui
         private void OnTextChanged(object sender, EventArgs e)
         {
             if (!suppressEvents)
-                setValue(getObject(), etoControl.Text);
+                setValue(getObject(), strToVal(etoControl.Text));
         }
 
         public void Update()
@@ -35,10 +42,10 @@ namespace Clarity.Ext.Gui.EtoForms.FluentGui
             if (etoControl.HasFocus)
                 return;
             var value = getValue(getObject());
-            if (etoControl.Text == value)
+            if (EqualityComparer<TVal>.Default.Equals(strToVal(etoControl.Text), value))
                 return;
             suppressEvents = true;
-            etoControl.Text = value;
+            etoControl.Text = valToStr(value);
             suppressEvents = false;
         }
     }
